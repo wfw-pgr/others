7bce10ec60738837319ca2797e3ad3e24be57c49
wfw-pgr <wfw.pgr@gmail.com>
2020-06-27 19:20:57 +0900

    add prepare__pole_mcoord

---
+++ prepare__pole_mcoord/prepare__pole_mcoord.py
@@ -0,0 +1,41 @@
+import numpy as np
+import os, sys, subprocess
+
+
+# ========================================================= #
+# ===  prepare__pole_mcoord                             === #
+# ========================================================= #
+
+def prepare__pole_mcoord():
+
+    # ------------------------------------------------- #
+    # --- [1] File copying                          --- #
+    # ------------------------------------------------- #
+    mdtRoot  = "/mnt/f/kent/mdt2/"
+    path     = "biaAnalysis/generate__ideal_mainField/dat/"
+    FileName = "bia_coordinates_mcoord.dat"
+    cooFile  = mdtRoot + path + FileName
+
+    cmd      = "cp {0} dat/".format( cooFile )
+    print( cmd )
+    subprocess.call( cmd.split() )
+
+    # ------------------------------------------------- #
+    # --- [2] File extraction                       --- #
+    # ------------------------------------------------- #
+
+    cooFile  = "dat/{0}".format( cooFile )
+    with open( cooFile, "r" ) as f:
+        Data = np.loadtxt( f )
+
+    outFile  = "dat/ems_pst.coord"
+    with open( outFile, "w" ) as f:
+        np.savetxt( f, Data, fmt="%15.8e" )
+
+
+# ========================================================= #
+# ===   実行部                                          === #
+# ========================================================= #
+
+if ( __name__=="__main__" ):
+    prepare__pole_mcoord()
